e5321580f4d336bf5db9202e9ad6544d7a17b5d6
oryaAronson <oryapreger@gmail.com>
2020-06-07 17:32:51 +0300

    1 test

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CartComponent } from './cart/cart.component';
+import { FeedComponent } from './feed/feed.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    FeedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
